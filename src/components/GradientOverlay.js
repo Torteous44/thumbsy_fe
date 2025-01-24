08ab77e295512ec35f20bfad147995df18ae820d
maxricodecastro <maxricodecastro@gmail.com>
2025-01-24 17:23:39 -0500

    changed fonts, added gradient overlay

---
+++ src/components/GradientOverlay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/GradientOverlay.css';
+
+const GradientOverlay = ({ height = '540px' }) => {
+  return (
+    <div 
+      className="gradient-overlay" 
+      style={{ height: height }}
+    />
+  );
+};
+
+export default GradientOverlay; 
